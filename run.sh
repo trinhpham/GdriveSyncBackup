37e6bfa5d13eb25d6b0eefe12401eeccb1f3699e
Trinh Pham <trinh.pham@logigear.com>
2019-03-31 21:37:16 +0700

    Update script

--- run.sh
+++ run.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
-cd "$BASEDIR"
-
-git pull
 
 pip install --user --upgrade oauth2client
 pip install --user --upgrade google-api-python-client
 
-INPUT_NAME=$@
-if ! [[ "$INPUT_NAME" == *\/* ]]
+PARENT_FOLDER=$2
+if [ -z "$PARENT_FOLDER" ]
 then
-  INPUT_NAME=`find . -maxdepth 1 -name $INPUT_NAME`
+    PARENT_FOLDER="."
 fi
 
+INPUT_NAME=$1
+if ! [[ "$INPUT_NAME" == *\/* ]]
+then
+  INPUT_NAME=`find $PARENT_FOLDER -maxdepth 1 -name $INPUT_NAME`
+fi
+
+
+cd "$BASEDIR"
+
+git pull
 python ./main/SyncBackup.py $INPUT_NAME
