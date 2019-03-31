4dec360cb435df615535ed949f5318e1ed957fdb
Trinh Pham <trinh.pham@logigear.com>
2019-03-31 21:27:27 +0700

    Update script

--- run.sh
+++ run.sh
@@ -5,7 +5,13 @@ cd "$BASEDIR"
 
 git pull
 
-pip install --upgrade oauth2client
-pip install --upgrade google-api-python-client
+pip install --user --upgrade oauth2client
+pip install --user --upgrade google-api-python-client
 
-python ./main/SyncBackup.py $@
+INPUT_NAME=$@
+if ! [[ "$INPUT_NAME" == *\/* ]]
+then
+  INPUT_NAME=`find . -maxdepth 1 -name $INPUT_NAME`
+fi
+
+python ./main/SyncBackup.py $INPUT_NAME
